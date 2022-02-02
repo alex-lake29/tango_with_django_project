416d58469386e5ff3ead47c2a0e3c7bede330bc8
Alex Lake <2576885L@student.gla.ac.uk>
2022-02-02 19:00:52 +0000

    Chapter 4: Changed to HTML

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -10,10 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+print(__file__)
+print(os.path.dirname(__file__))
+print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
