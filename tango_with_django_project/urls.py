70b0b302ca39dc788e531c7e28f1cf5616ff69b0
Alex Lake <2576885L@student.gla.ac.uk>
2022-02-13 21:04:38 +0000

    Chapter 4 all tests passed

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('rango/', views.index, name='index'),
     path('rango/', include('rango.urls')),
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
-    path('about/',views.about, name='about/'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
